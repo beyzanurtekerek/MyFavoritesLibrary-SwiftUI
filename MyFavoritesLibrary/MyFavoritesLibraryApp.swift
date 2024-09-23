4f2903a4fb10ac57d0d6bfb57547e954c50fa42b
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-23 18:23:05 +0300

    Initial Commit

---
+++ MyFavoritesLibrary/MyFavoritesLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoritesLibraryApp.swift
+//  MyFavoritesLibrary
+//
+//  Created by Beyza Nur Tekerek on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoritesLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
